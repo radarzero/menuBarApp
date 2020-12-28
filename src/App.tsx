93e81708c7bc167ca847a6a4337efc2c21933e53
Rohit Bhatnagar <42137382+radarzero@users.noreply.github.com>
2020-12-29 00:30:34 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import {
+  Grid,
+  Menu,
+  Segment,
+  Sidebar,
+  Icon,
+  Button,
+  Header,
+} from "semantic-ui-react";
+import Home from "./components/Home";
+import NewPage from "./components/NewPage";
+import "./App.css";
 
 function App() {
+  const [visible, setVisible] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid columns={1}>
+      <div className='nav-bar'>  
+      <Button secondary onClick={() => setVisible(true)}>
+        <Icon name="list" />
+      </Button>
+      </div>
+      <div className='nav-bar'>
+      <Header as="h2">
+        <Icon name="plug" />
+        <Header.Content>Uptime Guarantee</Header.Content>
+      </Header>
+      </div>
+      <Grid.Column>
+        <Sidebar.Pushable as={Segment.Group} raised>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            inverted
+            onHide={() => setVisible(false)}
+            vertical
+            visible={visible}
+            width="thin"
+          >
+            <Menu.Item>
+              <Link to="/home">Home</Link>{" "}
+            </Menu.Item>
+            <Menu.Item as="a">Games</Menu.Item>
+            <Menu.Item as="a">Channels</Menu.Item>
+          </Sidebar>
+
+          <Segment inverted padded>
+            <Switch>
+              <Route path="/home">
+                <Home />
+              </Route>
+              <Route exact path="/">
+                <NewPage />
+              </Route>
+            </Switch>
+          </Segment>
+        </Sidebar.Pushable>
+      </Grid.Column>
+    </Grid>
   );
 }
 
---
+++ src/components/NewPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewPage() {
+    return (
+        <div>
+           this is new page 
+        </div>
+    )
+}
+
+export default NewPage
