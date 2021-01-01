acfa64ef844bd2c7644adb1e81d9f001abba332f
Rohit Bhatnagar <42137382+radarzero@users.noreply.github.com>
2021-01-02 00:16:21 +0530

    home_page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button, Icon } from "semantic-ui-react";
+import "./home.css";
 
 export class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>this is Home</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="home-page">
+        <h1 className="home-heading">
+          <Icon name="universal access" />
+          Community finder
+        </h1>
+        <p className="home-content">
+          this is palce to find where you belong.choose the comunity u want to
+          signed up for then register yourself with community driven newsletter
+          which stay connect here the setup event and meetup to keep you updated
+          about the community so that in this digital age you will no feel
+          alone.
+        </p>
+        <Button primary color="blue" className="home-button">
+          Let Start!!
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
