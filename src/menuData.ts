dfb0727d13acaf0f6904be3cdea0b78fd4d3047f
Rohit Bhatnagar <42137382+radarzero@users.noreply.github.com>
2021-01-07 13:33:33 +0530

    one milestone

---
+++ src/menuData.ts
@@ -0,0 +1,41 @@
+const menuEle = [
+  {
+    name: "Home",
+    children:[],
+  },
+  {
+    name: "Artist",
+    children: [
+      {
+        name: "Dancer",
+      },
+      {
+        name: "Musician",
+      },
+    ],
+  },
+  {
+    name: "Fashion",
+    children: [
+      {
+        name: "Model",
+      },
+      {
+        name: "Indian Fashion",
+      },
+    ],
+  },
+  {
+    name: "Cooking",
+    children: [
+      {
+        name: "Chef",
+      },
+      {
+        name: "Japanese Recipies",
+      },
+    ],
+  },
+];
+
+export default menuEle;
